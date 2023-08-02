e16e5d2635e0bfdb5f969df15aa2ff82a1a4b42f
Rijughosh123 <riju.ghosh@brainiuminfotech.com>
2023-08-02 10:37:19 +0530

    template change

--- student_interface/urls.py
+++ student_interface/urls.py
@@ -3,7 +3,9 @@ from . import views
 urlpatterns = [
     path('home/', views.student_home), 
     path('login/', views.student_login), 
-    path('stu_dash/', views.student_dashboard), 
-    path('stu_home/', views.student_home), 
+
+    path('stu_home/', views.student_dash), 
     path('stu_course/', views.student_courses),
+    path('stu_p_course/', views.student_p_courses), 
+    path('student_trophy/', views.student_trophy), 
 ]
--- student_interface/views.py
+++ student_interface/views.py
@@ -1,32 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from .models import Profile
+from admin_interface1.models import CourseDetails,Technology
+from django.contrib.auth import authenticate, login
 
 def student_home(request):
     return render(request, 'home.html')
 
 def student_login(request):
+    print("hello")
+    if request.method == 'POST':
+        print("hello2")
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        print(username,password)
+        if user is not None:
+            login(request, user)
+            return redirect(student_dash) 
     return render(request, 'authenticate.html')
 
-def student_dashboard(request):
-    return render(request, 'authenticate.html')
-
-# def add_user(request):
-#     add = [
-#         {"username":"riju123", "email":"riju@gmail.com", "password":"12345", "first_name":"Riju", "last_name":"Ghosh"},
-#         {"username":"dipu123", "email":"dipu@gmail.com", "password":"12345", "first_name":"Dipu", "last_name":"Mondal"},
-#         {"username":"anirban123", "email":"anirban@gmail.com", "password":"12345", "first_name":"Anirban", "last_name":"Roy"}
-#         ]
-#     for i in add:
-#         user = User.objects.create(username=i['username'], email=i['email'], first_name=i['first_name'], last_name=i['last_name'])
-#         user.set_password(i['password'])
-#         Profile.objects.create(user_id=user.id, student=True)
-#         user.save()
-#     return HttpResponse("successfull")
-
-def student_home(request):
+#-------------------- after Authentication ---------------------
+def student_dash(request):
     return render(request, 'stu_dash/students_dashboard.html')
 
+def student_p_courses(request):
+    context = {}
+    all_course = CourseDetails.objects.all().values()
+    context.update({"all_course":all_course, "act":"p_courses"})
+    return render(request, 'stu_dash/students_p_course.html',context)
+
 def student_courses(request):
-    return render(request, 'stu_dash/students_course.html',{"l":[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+    context = {}
+    context.update({"act":"course"})
+    return render(request, 'stu_dash/students_courses.html',context)
+
+def student_trophy(request):
+    context ={}
+    context.update({"act":"trophy"})
+    return render(request, 'stu_dash/students_trophy.html',context)
