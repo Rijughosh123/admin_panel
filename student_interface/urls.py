60a22ad8b9df65949a8af7822fdbd3d33e4c57d0
Rijughosh123 <riju.ghosh@brainiuminfotech.com>
2023-08-14 18:00:26 +0530

    updated

--- student_interface/urls.py
+++ student_interface/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
 
     path('stu_home/', views.student_dash), 
     path('stu_course/', views.student_courses),
+    path('student_view_courses/', views.student_view_courses), 
+    path('student_today_class/', views.student_today_class),
     path('stu_p_course/', views.student_p_courses), 
     path('student_trophy/', views.student_trophy), 
 ]
--- student_interface/views.py
+++ student_interface/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import Profile
 from admin_interface1.models import CourseDetails,Technology
 from django.contrib.auth import authenticate, login
+from datetime import datetime
 
 def student_home(request):
     return render(request, 'home.html')
@@ -24,7 +25,20 @@ def student_login(request):
 
 #-------------------- after Authentication ---------------------
 def student_dash(request):
-    return render(request, 'stu_dash/students_dashboard.html')
+    user_id = 1
+    context = {}
+    data = {
+        "act":"home",
+        "user_name":"Riju Ghosh",
+        "today_date":datetime.now(),
+        "metting_details":[
+            {"metting_for":"For Django class", "time":"12.05 PM"},
+            {"metting_for":"For Angular class", "time":"15.05 PM"},
+            {"metting_for":"For Fun class", "time":"10.05 AM"}
+        ]
+    }
+    context.update(data)
+    return render(request, 'stu_dash/students_dashboard.html', context)
 
 def student_p_courses(request):
     context = {}
@@ -34,8 +48,19 @@ def student_p_courses(request):
 
 def student_courses(request):
     context = {}
-    context.update({"act":"course"})
+    context.update({"act":"course","l":[1,2,3,4,5,6,7]})
     return render(request, 'stu_dash/students_courses.html',context)
+
+def student_view_courses(request):
+    context = {}
+    context.update({"act":"course"})
+    return render(request, 'stu_dash/students_view_courses.html',context)
+
+def student_today_class(request):
+    context = {}
+    context.update({"act":"course"})
+    return render(request, 'stu_dash/students_today_class.html',context)
+
 
 def student_trophy(request):
     context ={}
